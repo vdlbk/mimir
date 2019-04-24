c7c9e39b6cf4c3532f2e6b0ea0b113b725fc6e9e
Valentin <valentindelbeke@hotmail.fr>
2019-04-24 13:10:07 +0200

    feat: Add method to compute check digits

--- iban.go
+++ iban.go
@@ -1,7 +1,9 @@
 package mimir
 
 import (
+	"fmt"
 	"math/big"
+	"strconv"
 	"strings"
 )
 
@@ -12,24 +14,9 @@ const (
 
 // IsIBANValid checks if the given IBAN is valid based on the country its belong to
 func IsIBANValid(iban string) (bool, error) {
-	// the given IBAN's length cannot be less than `minimumIBANLength`
-	if len(iban) < minimumIBANLength {
-		return false, ErrIBANTooshort
-	}
-
-	iban = strings.ToUpper(iban)
-
-	var conf configuration
-	var ok bool
-
-	// Extracts country code from the first 2 characters of the IBAN and if exists, get the country configuration associated with it.
-	if conf, ok = countriesConfiguration[iban[:2]]; !ok {
-		return false, ErrCountryCodeDoesNotExist
-	}
-
-	// Checks the length of the IBAN
-	if len(iban) != conf.IBANDefinition.Length {
-		return false, ErrIBANIncorrectLength
+	err := checkIBAN(iban)
+	if err != nil {
+		return false, err
 	}
 
 	// Move the `numberOfDigitRearranged` first characters to the end
@@ -51,4 +38,61 @@ func IsIBANValid(iban string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+// GetCheckDigits compute the check digits from a given IBAN.
+// Returns the computed digits, the IBAN set with the check digits or an error if something goes wrong
+func GetCheckDigits(iban string) (string, string, error) {
+	err := checkIBAN(iban)
+	if err != nil {
+		return "", "", err
+	}
+
+	// Replace the 2 checks digit by 00
+	iban = iban[:2] + "00" + iban[4:]
+
+	// Move the `numberOfDigitRearranged` first characters to the end
+	rearrangedIBAN := iban[numberOfDigitRearranged:] + iban[:numberOfDigitRearranged]
+
+	// Replace all possible letters by their integer value
+	sanitizedIBAN := ""
+	for _, c := range rearrangedIBAN {
+		sanitizedIBAN += alphabet[c]
+	}
+
+	iSanitizedIBAN := new(big.Int)
+	mod := new(big.Int)
+	iSanitizedIBAN.SetString(sanitizedIBAN, 10)
+	mod.Mod(iSanitizedIBAN, big.NewInt(97))
+
+	check := 98 - mod.Int64()
+
+	// Replace the 2 checks digit by the result
+	iban = iban[:2] + fmt.Sprintf("%02d", check) + iban[4:]
+
+
+	return strconv.Itoa(int(check)), iban, nil
+}
+
+func checkIBAN(iban string) error {
+	// the given IBAN's length cannot be less than `minimumIBANLength`
+	if len(iban) < minimumIBANLength {
+		return ErrIBANTooshort
+	}
+
+	iban = strings.ToUpper(iban)
+
+	var conf configuration
+	var ok bool
+
+	// Extracts country code from the first 2 characters of the IBAN and if exists, get the country configuration associated with it.
+	if conf, ok = countriesConfiguration[iban[:2]]; !ok {
+		return ErrCountryCodeDoesNotExist
+	}
+
+	// Checks the length of the IBAN
+	if len(iban) != conf.IBANDefinition.Length {
+		return ErrIBANIncorrectLength
+	}
+	return nil
 }
--- iban_test.go
+++ iban_test.go
@@ -12,3 +12,23 @@ func Test_IsIBANValid(t *testing.T) {
 		}
 	}
 }
+
+func Test_GetCheckDigits(t *testing.T) {
+	for _, configuration := range countriesConfiguration {
+
+		check, iban, err := GetCheckDigits(configuration.IBANDefinition.Example)
+		if err != nil {
+			t.Errorf("Unexpected error occured: %v", err)
+		}
+
+		if iban != configuration.IBANDefinition.Example {
+			t.Errorf("Output was incorrect, GetCheckDigit(%s)= %v, %v; want: %v, %v.",
+				configuration.IBANDefinition.Example,
+				check,
+				iban,
+				configuration.IBANDefinition.Example[2:4],
+				configuration.IBANDefinition.Example,
+			)
+		}
+	}
+}
